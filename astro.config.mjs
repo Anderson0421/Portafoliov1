58a0a0ad647a571b2d6ab72209a0fb7d7d8c2f83
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-10-29 09:23:53 -0500

    CamiosGenerales

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,11 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
     output: "server",
     adapter: vercel(),
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    integrations: [
+        tailwind({
+        // Ejemplo: Deshabilita la inyección de una importación básica de `base.css` en cada página.
+        // Muy útil si necesitas definir e/o importar tu propio `base.css` personalizado.
+        applyBaseStyles: false,
+    })],
 });
